f6d3cc8398715d83e3c2ec672f299f0b8979abd2
BillCI <bill@concreteinteractive.com>
2014-04-03 15:36:22 -0700

    localization language set ip

---
+++ Assets/Scripts/STATICS/LOCALIZATION.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class LOCALIZATION {
+	public enum language {english}
+
+	private static LOCALIZATION.language _currentLanguage = LOCALIZATION.language;
+	public static LOCALIZATION.language currentLanguage {
+		get {
+			return LOCALIZATION._currentLanguage;
+		}
+		set {
+			//TODO: implemnt some sort of language seltion;
+		}
+	}
+
+}
